dd9ffd4356779d6ee8a1f09ead3c14ad2e3c776e
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-10-21 16:56:04 -0300

    lidando com dados dinamicos em jsx

--- react-complete-guide/src/components/ExpenseItens.js
+++ react-complete-guide/src/components/ExpenseItens.js
@@ -1,12 +1,15 @@
 import './ExpenseItens.css'
 
 function ExpenseItens(){
+    const expenseDate = new Date(2021, 2, 28)
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.67
     return (
         <div className="expense-item">
-            <div>March 29th 2021</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>Car insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     )
