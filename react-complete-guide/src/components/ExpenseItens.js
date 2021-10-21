60b03a5be39d4c1df1ce106725448af7cc5a3f22
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-10-21 16:32:31 -0300

    JSX um pouco mais complexo
    Um componente em jsx só pode retornar uma raiz

--- react-complete-guide/src/components/ExpenseItens.js
+++ react-complete-guide/src/components/ExpenseItens.js
@@ -1,5 +1,13 @@
 function ExpenseItens(){
-    return (<h2>This is the expensive itens list</h2>)
+    return (
+        <div>
+            <div>March 29th 2021</div>
+            <div>
+                <h2>Car insurance</h2>
+                <div>$294.67</div>
+            </div>
+        </div>
+    )
 }
 
 export default ExpenseItens 
