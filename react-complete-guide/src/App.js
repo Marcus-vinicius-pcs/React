08d852599fd4f4b88c259423c0b7141ef99e8d51
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-10-22 18:46:58 -0300

    algumas alterações

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItens from "./components/ExpenseItens";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -26,26 +26,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItens
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      ></ExpenseItens>
-      <ExpenseItens
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      ></ExpenseItens>
-      <ExpenseItens
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      ></ExpenseItens>
-      <ExpenseItens
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      ></ExpenseItens>
+      <Expenses item={expenses} />
     </div>
   );
 }
---
+++ react-complete-guide/src/components/Expenses.js
@@ -0,0 +1,30 @@
+import ExpenseItens from './ExpenseItens'
+import './Expenses.css'
+function Expenses(props){
+    return(
+    <div className='expenses'>
+    <ExpenseItens
+        title={props.item[0].title}
+        date={props.item[0].date}
+        amount={props.item[0].amount}
+      ></ExpenseItens>
+      <ExpenseItens
+        title={props.item[1].title}
+        date={props.item[1].date}
+        amount={props.item[1].amount}
+      ></ExpenseItens>
+      <ExpenseItens
+        title={props.item[2].title}
+        date={props.item[2].date}
+        amount={props.item[2].amount}
+      ></ExpenseItens>
+      <ExpenseItens
+        title={props.item[3].title}
+        date={props.item[3].date}
+        amount={props.item[3].amount}
+      ></ExpenseItens>
+      </div>
+    )
+}
+
+export default Expenses
