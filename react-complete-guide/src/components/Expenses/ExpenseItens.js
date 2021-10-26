8c32cf2fb93d951fb61d8cea1351885a2f8c0151
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-10-26 12:46:27 -0300

    Usando states

--- react-complete-guide/src/components/Expenses/ExpenseItens.js
+++ react-complete-guide/src/components/Expenses/ExpenseItens.js
@@ -1,13 +1,21 @@
+import React, {useState} from 'react'
+
 import './ExpenseItens.css'
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/card'
 function ExpenseItens(props){
+    const [title, setTitle] = useState(props.title)
+
+    const clickHandler = () => {
+        setTitle('Updated!')
+    }
     return (
         <Card className="expense-item">
             <ExpenseDate date = {props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
+                <button onClick={clickHandler}>Change Title</button>
             </div>
         </Card>
     )
