f76b8f8270fbeaf91bc7e085c5809f37332169a0
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-10-27 12:06:16 -0300

    passando dados entre os components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -24,9 +24,14 @@ function App() {
     },
   ];
   
+  const addExpenseHandler = expense => {
+    console.log('App.js')
+    console.log(expense)
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses item={expenses} />
     </div>
   );
